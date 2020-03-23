d892f89f1209acdef982834d83af5333699b634c
Dominique Hazael-Massieux <dom@w3.org>
2020-03-23 11:41:55 +0100

    Extract and show data about comment reactions too

--- list-reactions.js
+++ list-reactions.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const issues = require("./issues.json");
 const comments = require("./comments.json");
 
-const results = [].concat(...Object.values(issues)).filter(i => i.reactions.total_count > 0).sort((a,b) => b.reactions.total_count - a.reactions.total_count);
+const issueReactions = [].concat(...Object.values(issues)).filter(i => i.reactions.total_count > 0).sort((a,b) => b.reactions.total_count - a.reactions.total_count);
 
-fs.writeFileSync('reactions.json', JSON.stringify(results, null, 2));
+const commentsReactions = [].concat(...Object.values(comments)).filter(c => c.reactions && c.reactions.total_count > 0).sort((a,b) => b.reactions.total_count - a.reactions.total_count);
+
+
+fs.writeFileSync('issue-reactions.json', JSON.stringify(issueReactions, null, 2));
+fs.writeFileSync('comment-reactions.json', JSON.stringify(commentsReactions, null, 2));
--- reactions.js
+++ reactions.js
@@ -9,13 +9,15 @@ const emojis = {
   "eyes": "👀"
 };
 
+const id = url => url.split('/').slice(4,7).join('/');
+
 const reactionLi = i => {
   const repo = i.href.split('/').slice(4,6).join('/');
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(repo + ": "));
   const a = document.createElement("a");
   a.href = i.href;
-  a.textContent = i.title;
+  a.textContent = i.title ? i.title : id(a.href);
   li.appendChild(a);
   li.appendChild(document.createTextNode(" (" + i.time.slice(0,10) + ")"));
   Object.keys(emojis).forEach(r => {
@@ -26,19 +28,26 @@ const reactionLi = i => {
   return li;
 };
 
-const recent = document.getElementById("recent");
+const recentIssues = document.getElementById("recent-issues");
+const recentComments = document.getElementById("recent-comments");
 const t = document.getElementById("top");
 const negative = document.getElementById("negative");
-fetch("reactions.json")
-  .then(r => r.json())
-  .then(issues => {
+Promise.all(["issue-reactions.json", "comment-reactions.json"].map(
+  f => fetch(f)
+            .then(r => r.json())
+))
+  .then(([issues, comments]) => {
     issues.filter(i => i.state === "open").slice(0, 40)
       .forEach(i => {
         t.appendChild(reactionLi(i));
     });
     issues.sort((a,b) => -a.time.localeCompare(b.time)).slice(0, 20)
       .forEach(i => {
-        recent.appendChild(reactionLi(i));
+        recentIssues.appendChild(reactionLi(i));
+    });
+    comments.sort((a,b) => -a.time.localeCompare(b.time)).slice(0, 20)
+      .forEach(i => {
+        recentComments.appendChild(reactionLi(i));
     });
     issues.sort((a,b) => -(a.reactions["-1"] + a.reactions["confused"]) + (b.reactions["-1"] + b.reactions["confused"])).slice(0, 40)
       .forEach(i => {
